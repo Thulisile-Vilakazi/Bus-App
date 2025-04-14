522ce72daf981bc1ebe1ee2ebd9e0014c874a22a
Thulisile-Vilakazi <thuliveronicavilakazi9@gmail.com>
2025-04-14 21:24:02 +0200

    Added a dictionary of the data to be extracted from the csv

---
+++ data/main.py
@@ -0,0 +1,11 @@
+schedule_dict = {
+  "trip_name": "Trip 1",
+  "stops": [
+    {"stop": "FISANTEKRAAL", "time": "05:30a"},
+    {"stop": "DURBANVILLE", "time": "--"},
+    {"stop": "N1 FREEWAY", "time": "via"},
+    {"stop": "VOORTREKKER RD", "time": "via"},
+    {"stop": "CAPE TOWN", "time": "06:20a"},
+    {"stop": "CLARA ANNA FONTEIN", "time": "--"}
+  ]
+}
